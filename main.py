5b63689b882d0e8d512353ebb6d18ad1d43bc496
bkaminskthatsme <bdict.kam@hotmail.com>
2023-08-31 00:17:18 -0600

    added movement

--- main.py
+++ main.py
@@ -9,12 +9,18 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
-game_on = True
+screen.listen()
+screen.onkey(key="w", fun=snake.up)
+screen.onkey(key="a", fun=snake.left)
+screen.onkey(key="s", fun=snake.down)
+screen.onkey(key="d", fun=snake.right)
 
+
+game_on = True
 while game_on:
     snake.move()
     screen.update()
-    time.sleep(0.3)
+    time.sleep(0.1)
 
 
 
--- snake.py
+++ snake.py
@@ -15,7 +15,6 @@ class Snake:
             new_segment.shape("square")
             new_segment.goto(x=0 - 20 * seg, y=0)
             self.snake.append(new_segment)
-            print(self.snake)
 
     def move(self):
         for segment in range(len(self.snake) - 1, 0, -1):
@@ -23,3 +22,19 @@ class Snake:
             new_y = self.snake[segment - 1].ycor()
             self.snake[segment].goto(new_x, new_y)
         self.snake[0].forward(MOVE_DISTANCE)
+
+    def up(self):
+        if self.snake[0].heading() != 270:
+            self.snake[0].setheading(90)
+
+    def left(self):
+        if self.snake[0].heading() != 0:
+            self.snake[0].setheading(180)
+
+    def right(self):
+        if self.snake[0].heading() != 180:
+            self.snake[0].setheading(0)
+
+    def down(self):
+        if self.snake[0].heading() != 90:
+            self.snake[0].setheading(270)
